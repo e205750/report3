566bef3aa81283c60b8a041312d5e2d46cd76149
Naka Kouto <e205750@ie.u-ryukyu.ac.jp>
2024-02-14 15:32:41 +0900

    Update Hero and Enemy classes, add new files

--- app/src/main/java/jp/ac/uryukyu/ie/e205750/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205750/Enemy.java
@@ -8,38 +8,15 @@ package jp.ac.uryukyu.ie.tnal;
  *  boolean dead; //敵の生死状態。true=死亡。
  * Created by tnal on 2016/11/13.
  */
-public class Enemy {
-    public String name;
-    public int hitPoint;
-    public int attack;
-    public boolean dead;
-
+public class Enemy extends LivingThing {
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
-     * @param name モンスター名
+     * @param _name モンスター名
      * @param maximumHP モンスターのHP
-     * @param attack モンスターの攻撃力
+     * @param _attack モンスターの攻撃力
      */
-    public Enemy (String name, int maximumHP, int attack) {
-        this.name = name;
-        hitPoint = maximumHP;
-        this.attack = attack;
-        dead = false;
-        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
-    }
-
-    /**
-     * Heroへ攻撃するメソッド。
-     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
-     * @param hero 攻撃対象
-     */
-    public void attack(Hero hero){
-        if(dead == false){
-
-        int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
-        hero.wounded(damage);
-        }
+    public Enemy (String _name, int maximumHP, int _attack) {
+        super(_name, maximumHP, _attack);
     }
 
     /**
@@ -47,11 +24,13 @@ public class Enemy {
      * 指定されたダメージを hitPoint から引き、死亡判定を行う。
      * @param damage 受けたダメージ
      */
+    @Override
     public void wounded(int damage){
-        hitPoint -= damage;
-        if( hitPoint < 0 ) {
-            dead = true;
-            System.out.printf("モンスター%sは倒れた。\n", name);
+        int hitPoint = getHitPoint();
+        setHitPoint(hitPoint -= damage);
+        if( getHitPoint() <= 0 ) {
+            setDead(true);
+            System.out.printf("モンスター%sは倒れた。\n", getName());
         }
     }
 
